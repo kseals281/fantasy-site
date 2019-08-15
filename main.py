f5ccfc28357c6299983118348e54efe86841a6e6
Khari Seals <kseals281@gmail.com>
2019-08-15 10:26:25 -0400

    User session is now stored and user login is recognized on every page. Updated the profile button in the top right to be login/register when no user login has occurred but {user} profile when one has.

--- main.py
+++ main.py
@@ -1,32 +1,43 @@
 from flask import Flask, escape, url_for, render_template, request, redirect
+from flask import make_response, session
 from pymongo import MongoClient
 import hashlib
+import os
 
 app = Flask(__name__)
+
+app.secret_key = os.environ.get('SECRET_KEY')
 
 client = MongoClient()
 
 db = client.test_database
 
 @app.route('/', methods=['POST', 'GET'])
-def index(name=None):
-  return render_template('indexpy.html', name=name)
+def index():
+  if 'username' in session:
+    username = session['username']
+  print('Logged in as %s' % escape(session['username']))
+  return render_template('indexpy.html', name=username)
 
-@app.route('/create', methods=['POST', 'GET'])
+@app.route('/register', methods=['POST', 'GET'])
 def create_account():
   accounts = db.accounts
   if request.method == 'POST':
     username = request.values.get('username')
     password = request.values.get('password')
+    repeated_password = request.values.get('repeatpassword')
     if check_for_username(username):
       return render_template('register.html', usernameTaken=True)
-    hashedPassword = hashlib.sha256(password.encode()).hexdigest()
+    if password != repeated_password:
+      return render_template('register.html', passwords_dont_match=True)
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
     new_account = {
       'username': username,
-      'password': hashedPassword
+      'password': hashed_password
     }
     accounts.insert_one(new_account).inserted_id
-    return render_template('indexpy.html', user=username)
+    session['username'] = username
+    return redirect(url_for('profile'))
   return render_template('register.html', usernameTaken=False)
 
 def check_for_username(username):
@@ -40,8 +51,11 @@ def login():
   if request.method == 'POST':
     username = request.values.get('username')
     password = request.values.get('password')
+    if not check_for_username(username):
+      return render_template('login.html', firstattempt=False)
     if verify_login(username, password):
-      return render_template('success.html', user=username, message='login')
+      session['username'] = username
+      return redirect(url_for('profile'))
     return render_template('login.html', firstattempt=False)
   else:
     return render_template('login.html', firstattempt=True)
@@ -55,6 +69,19 @@ def verify_login(username, password):
   if account_to_verify['password'] == password:
     return True
   return False
+
+@app.route('/profile')
+def profile():
+  if 'username' not in session:
+    return redirect(url_for('login'))
+  username = session['username']
+  return render_template('profile.html', name=username)
+
+def check_login():
+  username = request.cookies.get('user')
+  if username == None:
+    return None
+  return username
 
 @app.route('/reset')
 def reset():
