1aa6afbf0bd4538f00cfaeadc3170fa50944607a
Khari Seals <kseals281@gmail.com>
2019-08-15 16:21:06 -0400

    User accounts show racers of current user and working on allowing them to select based on runners in db

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 
-app.secret_key = os.environ.get('SECRET_KEY')
+app.secret_key = b'\x80 \xa55\x98/\xba\xa8\xeb\x1ec\xfc\xee5l\xd1'
 
 client = MongoClient()
 
@@ -33,10 +33,18 @@ def create_account():
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
     new_account = {
       'username': username,
-      'password': hashed_password
+      'password': hashed_password,
+      'racer1': None,
+      'racer2': None,
+      'racer3': None
     }
     accounts.insert_one(new_account).inserted_id
     session['username'] = username
+    session['account'] = {
+      'racer1': None,
+      'racer2': None,
+      'racer3': None
+    }
     return redirect(url_for('profile'))
   return render_template('register.html', usernameTaken=False)
 
@@ -70,15 +78,35 @@ def verify_login(username, password):
     return True
   return False
 
-@app.route('/profile')
+@app.route('/profile', methods=['POST', 'GET'])
 def profile():
   if 'username' not in session:
     return redirect(url_for('login'))
+  racers = db.racers
+  racers_list = racers.find()
   username = session['username']
-  return render_template('profile.html', name=username)
+  current_user = session['account']
+  if request.method == 'POST':
+    get_user_racers()
+  racer1 = current_user['racer1']
+  racer2 = current_user['racer2']
+  racer3 = current_user['racer3']
+  return render_template(
+    'profile.html', name=username, racer1=racer1, racer2=racer2, racer3=racer3,
+    runner_list=racers_list)
+
+def get_user_racers():
+  racers = db.racers
+  racer1 = (request.values.get('racer1')).lower()
+  racer2 = (request.values.get('racer2')).lower()
+  racer3 = (request.values.get('racer3')).lower()
+  runners = [racer1, racer2, racer3]
+  print(racers.find({'name': racer2}))
+  for runner in runners:
+    if runner == racers.find({'name': runner}):
+      print("Racer %s not found" % runner)
 
 def check_login():
-  username = request.cookies.get('user')
   if username == None:
     return None
   return username
@@ -87,7 +115,30 @@ def check_login():
 def reset():
   accounts = db.accounts
   accounts.delete_many({'username': 'foo'})
+  racers = db.racers
+  racers.delete_many({})
   return redirect(url_for('create_account'))
+
+@app.route('/upload', methods=['POST', 'GET'])
+def upload_race():
+  if request.method == 'POST':
+    racers = db.racers
+    racers.delete_many({})
+    for j in range(1, 5):
+      for i in range(1, 9):
+        team = ('team' + str(j)).lower()
+        racer = ('racer' + str(i)).lower()
+        name = request.values.get(team+racer)
+        current_racer = {
+          'team': team,
+          'name': name,
+        }
+        print('Current racer name: %s' % current_racer['name'])
+        if current_racer['name'] != '':
+          print(current_racer)
+          racers.insert_one(current_racer).inserted_id
+    return render_template('main.html')
+  return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
